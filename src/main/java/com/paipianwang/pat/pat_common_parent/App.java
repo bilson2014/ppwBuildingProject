50cce298866ca6f9df5126a818f8fab391f28e38
Jacky1100 <609615907@qq.com>
2016-12-30 18:48:08 +0800

    Jack update

---
+++ src/main/java/com/paipianwang/pat/pat_common_parent/App.java
@@ -0,0 +1,13 @@
+package com.paipianwang.pat.pat_common_parent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
